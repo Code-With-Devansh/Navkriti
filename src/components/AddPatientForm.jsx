ecdebf4e1168aaa355d7122a1d37f589dca7d64a
Code-With-Devansh <95998107+Code-With-Devansh@users.noreply.github.com>
2025-10-21 12:40:48 +0530

    added sidebar to the add patient page

--- src/components/AddPatientForm.jsx
+++ src/components/AddPatientForm.jsx
@@ -5,10 +5,12 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import PatientRegister from "./PatientRegister";
+import SideBar from "./SideBar";
 const AddPatient = () => {
 
   return (
     <div className="form-container patient-register-container">
+      <SideBar/>
       <div className="right">
         <PatientRegister/>
       </div>
